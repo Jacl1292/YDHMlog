1b3fde960fc5cf376275457e2c3ed6cc2d39415b
jose armando carballo <jacl12592@gmail.com>
2024-04-21 09:04:30 -0400

    ruta root

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('../root'));
 root.render(
   <React.StrictMode>
     <App />
